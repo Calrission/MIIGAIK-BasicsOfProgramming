0fbe38a11bef5a42fd38a58324b8a155a220fec1
artemii <kavrese@gmail.com>
2024-06-04 22:37:52 +0300

    completed task7

--- individual/task_7/main.py
+++ individual/task_7/main.py
@@ -20,7 +20,9 @@ class MainWindow(QMainWindow):
     def set_events(self):
         self.ui.action_2.triggered.connect(self.about_authors)
         self.ui.pushButton_2.clicked.connect(self.clear_fields)
-        self.ui.pushButton.clicked.connect(self.count_integral)
+        self.ui.pushButton.clicked.connect(lambda: self.count_integral(type=1))
+        self.ui.pushButton_4.clicked.connect(lambda: self.count_integral(type=2))
+        self.ui.pushButton_3.clicked.connect(lambda: self.count_integral(type=0))
 
     def set_img(self):
         self.ui.label_6.setPixmap(QPixmap("assets/img.png"))
@@ -28,7 +30,17 @@ class MainWindow(QMainWindow):
     def del_img(self):
         self.ui.label_6.setPixmap(QPixmap())
 
-    def count_integral(self):
+    def count_integral(self, type: int):
+        def x_for_left():
+            return a + h * i
+
+        def x_for_right():
+            return a + h * (i + 1)
+
+        def x_for_center():
+            return a + h * (i + 0.5)
+
+        func_x = [x_for_left, x_for_center, x_for_right][type]
         s1, s2 = 1, 0  # Инициализация переменных s1 и s2, которые будут хранить суммы для последовательных итераций
         iter = 0  # Счетчик итераций
         a, b, eps = int(self.ui.lineEdit.text()), int(self.ui.lineEdit_2.text()), float(self.ui.lineEdit_3.text().replace(',', '.'))
@@ -40,7 +52,7 @@ class MainWindow(QMainWindow):
             iter += 1  # Увеличение счетчика итераций
             i = 0  # Счетчик для цикла по разбиениям
             while i < n:  # Цикл по всем прямоугольникам
-                x = a + h * (i + 0.5)  # Вычисление середины текущего прямоугольника
+                x = func_x()  # Вычисление середины текущего прямоугольника
                 # Вычисление значения функции в середине прямоугольника
                 k = (0.5 + sqrt(x))/(1 + math.log(x, math.e)**2)
                 s2 += k * h  # Добавление площади текущего прямоугольника к сумме
@@ -48,6 +60,7 @@ class MainWindow(QMainWindow):
         ans = f'\nЗначение интеграла: {s2:.3f}\nКоличество итераций: {iter}'
         self.ui.label_5.setText(ans)
         self.set_img()
+
 
     def about_authors(self):
         QMessageBox.about(self, "Об авторах", "Программа была выполнена студентом:\n Струков Артемий 2023-ФГиИБ-ПИ-1б")
